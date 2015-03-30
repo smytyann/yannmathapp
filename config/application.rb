a7cd2c72d261d1f6e4f1b1bfc54190d487d0b9af
Yann <smytyann@gmail.com>
2015-03-30 15:50:41 +0000

    again

--- config/application.rb
+++ config/application.rb
@@ -1,11 +1,11 @@
 require File.expand_path('../boot', __FILE__)
 
 require 'rails/all'
-
+config.assets.initialize_on_precompile = false
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
+
 module YannMathapp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
